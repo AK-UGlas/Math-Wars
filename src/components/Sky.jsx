50adb46c8a00426ab9941c1968518eccb15cc23c
Allen_K <allenkelly278@gmail.com>
2021-06-30 09:07:40 +0100

    add background components: sky and ground

---
+++ src/components/Sky.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { skyAndGroundWidth, gameHeight } from '../utils/constants';
+
+const Sky = () => {
+  const skyStyle = {
+    fill: "url(#skyGradient)",
+  };
+  
+  return (
+    <rect
+      style={skyStyle}
+      x={skyAndGroundWidth / -2}
+      y={100 - gameHeight}
+      width={skyAndGroundWidth}
+      height={gameHeight}
+    />
+  );
+};
+
+export default Sky;
