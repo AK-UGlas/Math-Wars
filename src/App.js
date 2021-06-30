e6a1b8f09dc5c0cbeeb8d6d787cc5be72e63e878
Allen_K <allenkelly278@gmail.com>
2021-06-30 08:53:36 +0100

    update App to urender Canvas component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import React, {useEffect} from "react";
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Game from './containers/Game';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './components/Canvas';
 
-function App() {
+class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
 
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  render() {
     return (
-      <Game />
+      <Canvas
+        angle={this.props.angle}
+        gameState={this.props.gameState}
+        startGame={this.props.startGame}
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
+  }
 }
 
 App.propTypes = {
   angle: PropTypes.number.isRequired,
+
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+
+    bombObjects: PropTypes.arrayOf(PropTypes.shape({
+
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+
+      equation: PropTypes.shape({
+        xval: PropTypes.number.isRequired,
+        yval: PropTypes.number.isRequired,
+        result: PropTypes.number.isRequired,
+        op: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+      }).isRequired,
+
+      id: PropTypes.number.isRequired,
+
+    })).isRequired,
+
+  }).isRequired,
+
   moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default App;
