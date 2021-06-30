3ace7e4c607d0ffa8dac33ed4a20f82dac7db7aa
Allen_K <allenkelly278@gmail.com>
2021-06-30 09:12:42 +0100

    update reducer to handle start game action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,29 @@
-import { ACTIONS, moveObjects } from "../actions";
+import { MOVE_OBJECTS, startGame, START_GAME } from '../actions';
+import moveObjects from './moveObjects';
+
+const initialGameState = {
+  started: false,
+  targetsDestroyed: 0,
+  targetsLoaded: 0,
+  lives: 3,
+  bombObjects: [],
+  lastObjectCreatedAt: new Date(),
+}
 
 const initialState = {
-    angle: 45,
+  angle: 45,
+  gameState: initialGameState,
 };
 
-function reducer(state=initialState, action) {
-    switch (action.type) {
-        case ACTIONS.MOVE_OBJECTS:
-            return moveObjects(state, action);
-        default:
-            return state;
-    }
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS:
+      return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
+    default:
+      return state;
+  }
 }
 
 export default reducer;
